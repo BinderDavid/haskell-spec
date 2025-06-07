b9e8771eaf6395c13238039ef3bf118874201521
David Binder <david.binder@uni-tuebingen.de>
2025-06-07 16:04:00 +0200

    Finish SemanticTypes

--- HaskellSpec/SemanticTypes.lean
+++ HaskellSpec/SemanticTypes.lean
@@ -1,3 +1,5 @@
+import HaskellSpec.Names
+
 /-!
 # Semantic Types
 
@@ -6,39 +8,24 @@ Semantic types are defined in Fig. 4 of the paper.
 
 /--
 ```text
-σ ∈ Type Scheme →  ∀ α₁ … αₖ.θ ⇒ τ
+κ ∈ Kind → *
+         | κ → κ
 ```
 -/
-inductive TypeSchemeS : Type where
-/--
-```text
-θ ∈ Context → (Γ₁ τ₁, … , Γₙ τₙ)
-```
--/
-inductive ContextS : Type where
+inductive KindS : Type where
+  | Star : KindS
+  | Fun : KindS → KindS → KindS
 
 /--
 ```text
-τ ∈ Type → α
-         | χ
-         | τ₁ τ₂
+C ∈ Original class name → B
+                        | M!B
 ```
 -/
-inductive TypeS : Type where
+inductive Original_Class_NameS : Type where
+  | Unqualified : Class_Name → Original_Class_NameS
+  | Qualified : Module_Name → Class_Name → Original_Class_NameS
 
-/--
-```text
-χ ∈ Type constructor → Tᵏ
-```
--/
-inductive Type_ConstructorS : Type where
-
-/--
-```text
-α ∈ Type variable → uᵏ
-```
--/
-inductive Type_VariableS : Type where
 
 /--
 ```text
@@ -46,6 +33,8 @@ inductive Type_VariableS : Type where
 ```
 -/
 inductive Class_NameS : Type where
+  | Mk : Original_Class_NameS → KindS → Class_NameS
+
 
 /--
 ```text
@@ -55,19 +44,52 @@ T ∈ Original type name → S
 ```
 -/
 inductive Original_Type_NameS : Type where
+  | Unqualified : Type_Name → Original_Type_NameS
+  | Qualified : Module_Name → Type_Name → Original_Type_NameS
+  | Special : Original_Type_NameS
+
 
 /--
 ```text
-C ∈ Original class name → B
-                        | M!B
+χ ∈ Type constructor → Tᵏ
 ```
 -/
-inductive Original_Class_NameS : Type where
+inductive Type_ConstructorS : Type where
+  | Mk : Original_Type_NameS → KindS → Type_ConstructorS
 
 /--
 ```text
-κ ∈ Kind → *
-         | κ → κ
+α ∈ Type variable → uᵏ
 ```
 -/
-inductive KindS : Type where
+inductive Type_VariableS : Type where
+  | Mk : Type_Variable → KindS → Type_VariableS
+
+/--
+```text
+τ ∈ Type → α
+         | χ
+         | τ₁ τ₂
+```
+-/
+inductive TypeS : Type where
+  | Variable : Type_VariableS → TypeS
+  | TypeConstructor : Type_ConstructorS → TypeS
+  | App : TypeS → TypeS → TypeS
+
+
+/--
+```text
+θ ∈ Context → (Γ₁ τ₁, … , Γₙ τₙ)
+```
+-/
+inductive ContextS : Type where
+  | Mk : List (Class_NameS × TypeS) → ContextS
+
+/--
+```text
+σ ∈ Type Scheme →  ∀ α₁ … αₖ.θ ⇒ τ
+```
+-/
+inductive TypeSchemeS : Type where
+  | Forall : List Type_VariableS → ContextS → TypeS → TypeSchemeS
