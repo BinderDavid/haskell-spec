2b752f904ed24bbff88304ffe78291857e111847
David Binder <david.binder@uni-tuebingen.de>
2025-06-07 14:27:04 +0200

    Add empty files for Declarations, Expressions and Names

--- HaskellSpec.lean
+++ HaskellSpec.lean
@@ -1,3 +1,3 @@
--- This module serves as the root of the `HaskellSpec` library.
--- Import modules here that should be built as part of the library.
-import HaskellSpec.Basic
+import HaskellSpec.Declarations
+import HaskellSpec.Expressions
+import HaskellSpec.Names
