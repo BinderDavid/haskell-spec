528427ac7d59aac8123571b67c7cedd0bb661628
David Binder <david.binder@uni-tuebingen.de>
2025-06-07 17:18:07 +0200

    Add special type constructors

--- HaskellSpec/Names.lean
+++ HaskellSpec/Names.lean
@@ -5,8 +5,18 @@ There is a qualified and an unqualified variant for all names.
 We call them X and QX, respectively.
 
 Names are defined in Table 1 and Fig. 3 in the paper
-
 -/
+
+/--
+`M`
+-/
+inductive Module_Name : Type where
+
+/--
+`M`
+-/
+inductive QModule_Name : Type where
+
 
 /--
 `v`
@@ -30,14 +40,38 @@ inductive Constructor : Type where
 inductive QConstructor : Type where
 
 /--
-`S`
+```text
+Σ ∈ Special type constructor → ()
+                             | (k)
+                             | []
+                             | (->)
+```
 -/
-inductive Type_Name : Type where
+inductive Special_Type_Constructor : Type where
+  | Unit : Special_Type_Constructor
+  | Tuple : Nat → Special_Type_Constructor
+  | List : Special_Type_Constructor
+  | Fun : Special_Type_Constructor
 
 /--
-`T`
+```text
+S ∈ Type constructor
+```
+-/
+inductive Type_Name : Type where
+  | Mk : String -> Type_Name
+
+/--
+```text
+T ∈ Qualified type constructor → S
+                               | M.S
+                               | Σ
+```
 -/
 inductive QType_Name : Type where
+  | Unqualified : Type_Name → QType_Name
+  | Qualified : Module_Name → Type_Name → QType_Name
+  | Special : Special_Type_Constructor → QType_Name
 
 /--
 `u`
@@ -58,13 +92,3 @@ inductive Class_Name : Type where
 `C`
 -/
 inductive QClassName : Type where
-
-/--
-`M`
--/
-inductive Module_Name : Type where
-
-/--
-`M`
--/
-inductive QModule_Name : Type where
