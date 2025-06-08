c2f0579008eaa2526bd7ca00dc5c7b72622c4cbe
Björn Gohla <b.gohla@gmx.de>
2025-06-08 23:03:34 +0100

    add definitions for most operations on environments

--- HaskellSpec/Environments.lean
+++ HaskellSpec/Environments.lean
@@ -7,6 +7,45 @@ namespace Environment
 def Env (name : Type) (info : Type) : Type :=
   List (name × info)
 
+/--
+Operations on Environments
+Section 2.7
+-/
+def dom (env : Env name info) : List name :=
+  List.map Prod.fst env
+
+def remove [BEq name] (env : Env name info) (names : List name) : (Env name info) :=
+  List.filter pred env
+  where pred := (Bool.not ∘ (List.contains names) ∘ Prod.fst)
+
+def restrict [BEq name] (env : Env name info) (names : List name) : (Env name info) :=
+  List.filter pred env
+  where pred := (List.contains names ∘ Prod.fst)
+
+def intersect [BEq t] (l l' : List t) : (List t) :=
+  List.filter (List.contains l') l
+
+def oplus [BEq name] [BEq info] (env env' : Env name info) (_ : (intersect (dom env) (dom env') = empty)) : (Env name info) :=
+  List.append env env'
+
+def oplusbar [BEq name] (env env' : Env name info) (_ : (restrict env (dom env') = restrict env' (dom env))) : (Env name info) :=
+  List.append env env'
+
+def oplusarrow [BEq name] [BEq info] (env env' : Env name info) : (Env name info) :=
+  List.append (remove env (dom env')) env'
+
+def oplustilde (env env' : Env name info) : (Env name info) :=
+  List.append env env'
+
+def unQual [Unqual name] : Env name info -> Env name info :=
+  List.map (λ(n, i) => (Unqual.unQual n, i))
+
+def justQs [IsQual name] : Env name info -> Env name info :=
+  List.filter (IsQual.isQual ∘ Prod.fst)
+
+def justSingle [BEq name] [BEq info] : Env name info -> Env name info :=
+  sorry -- TODO My first attempt at defining this did not satisfy the
+        -- termination checker.
 
 /--
 Class environment
