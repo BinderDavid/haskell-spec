da4923187431b71c92000cfed85c1a2d17b382dc
David Binder <david.binder@uni-tuebingen.de>
2025-06-08 14:54:45 +0200

    Add judgement forms up to fig 28

--- HaskellSpec/Elaboration/Modules.lean
+++ HaskellSpec/Elaboration/Modules.lean
@@ -22,23 +22,36 @@ inductive module : Environment.ME → Source.Module → Target.Module → Enviro
 
 /--
 Cp. Fig 15
+```text
+M, FE ⊢ body ⇝ typeDecls;binds : FE, SE
+```
 -/
 inductive body : Module_Name → Environment.FE → Source.ModuleBody → Target.ModuleBody → Environment.FE → Environment.SE → Prop where
 
 /--
 Cp. Fig 17
+```text
+GE, IE, VE ⊢ ctDecls ⇝ typeDecls; binds : FE
+```
 -/
 inductive ctdecls : Environment.GE → Environment.IE → Environment.VE → Source.ClassesAndTypes → Target.ClassesAndTypes → Environment.FE → Prop where
 
 
 /--
 Cp. Fig 18
+```text
+TE, h ⊢ t : τ
+```
 -/
-inductive type : Environment.TE → Source.TypeExpression → SemanticTypes.TypeS → Prop where
-
+inductive type : Environment.TE → Int
+               → Source.TypeExpression
+               → SemanticTypes.TypeS → Prop where
 
 /--
 Cp. Fig 19
+```text
+GE, IE, VE ⊢ ctDecl ⇝ typeDecls; binds : ⟨ CE, TE, KE, IE, VE⟩
+```
 -/
 inductive ctdecl : Environment.GE → Environment.VE → Environment.IE
                  → Source.ClassOrType
@@ -48,6 +61,9 @@ inductive ctdecl : Environment.GE → Environment.VE → Environment.IE
 
 /--
 Cp. Fig 20
+```text
+TE, θ, τ ⊢ conDecl ⇝ conDecl : DE, VE, LE, θ
+```
 -/
 inductive condecl : Environment.TE → SemanticTypes.Context → SemanticTypes.TypeS
                   → Source.ConstructorDecl
@@ -55,3 +71,97 @@ inductive condecl : Environment.TE → SemanticTypes.Context → SemanticTypes.T
                   → Environment.DE → Environment.VE → Environment.LE
                   → SemanticTypes.Context
                   → Prop where
+
+
+/--
+Cp. Fig 22
+```text
+IE, φ ⊢ τ_old, UE, τ_new
+```
+TODO: UE and φ are still not formalized
+-/
+inductive lcon : Environment.IE → SemanticTypes.TypeS → SemanticTypes.TypeS → Prop where
+
+
+/--
+Cp. Fig 23
+```text
+GE, IE, VE ⊢ ctDecl ⇝ typeDecls; binds : FE
+```
+-/
+inductive ctDecl : Environment.GE → Environment.IE → Environment.VE
+                 → Source.ClassOrType
+                 → Target.ClassOrType
+                 → Prop where
+
+/--
+Cp. Fig 24
+```text
+GE ⊢ sigs : VE
+```
+-/
+inductive sigs : Environment.GE → Source.Signatures → Environment.VE → Prop where
+
+/--
+Cp. Fig 24
+```text
+GE ⊢ sig : VE
+```
+-/
+inductive sig : Environment.GE → Source.Signature → Environment.VE → Prop where
+
+/--
+Cp. Fig 25
+```text
+CE, TE, h ⊢ cx : θ
+```
+-/
+inductive context : Environment.CE → Environment.TE → Int
+                  → Source.Context
+                  → SemanticTypes.Context
+                  → Prop where
+
+/--
+Cp. Fig 25
+```text
+CE, TE, h ⊢ class : Γ τ
+```
+-/
+inductive classR : Environment.CE → Environment.TE → Int
+                 → Source.ClassAssertion
+                 → SemanticTypes.Class_Name
+                 → SemanticTypes.TypeS
+                 → Prop where
+
+
+/--
+Cp. Fig 26
+```text
+GE, IE, VE ⊢ instDecls ⇝ binds : IE
+```
+-/
+inductive instDecls : Environment.GE → Environment.IE → Environment.VE → Source.InstanceDecls → Target.InstanceDecls →Environment.IE → Prop where
+
+/--
+Cp. Fig 26
+```text
+GE, IE, VE ⊢ instDecl ⇝ binds : IE
+```
+-/
+inductive instDecl : Environment.GE → Environment.IE → Environment.VE → Source.InstanceDecl → Target.InstanceDecl → Environment.IE → Prop where
+
+/--
+Cp. Fig 27
+```text
+GE, IE, VE ⊢ bind ⇝ fbind : VE
+```
+-/
+inductive method : Environment.GE → Environment.IE → Environment.VE → Source.Binding → Target.FieldBinding → Environment.VE → Prop where
+
+/--
+Cp. Fig 28
+```text
+IE ⊢ e : (Γ₁ τ₁,…,Γₙ τₙ)
+```
+-/
+inductive dict : Environment.IE → Source.Expression → Prop where
