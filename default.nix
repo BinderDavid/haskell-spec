9a9eae52e810afcfee28024f7b22815c75ffa102
Björn Gohla <51823984+cgohla@users.noreply.github.com>
2025-06-08 14:21:42 +0100

    make lean4 mode available in the nix shell (#11)

--- default.nix
+++ default.nix
@@ -2,10 +2,20 @@ let nixpkgs = import (import ./nix/sources.nix).nixpkgs
   { config.allowUnfree = true; };
 in with nixpkgs;
 rec {
+  lean4mode = fetchFromGitHub {
+    repo = "lean4-mode";
+    owner = "leanprover-community";
+    rev = "1388f9d1429e38a39ab913c6daae55f6ce799479";
+    hash = "sha256-6XFcyqSTx1CwNWqQvIc25cuQMwh3YXnbgr5cDiOCxBk=";
+  };
   shell = mkShell {
     nativeBuildInputs = [ elan steam-run ];
     shellHook =
      ''
+      ## It should be enough to add
+      ## (require 'lean4-mode)
+      ## to the .emacs file.
+      export EMACSLOADPATH="${lean4mode}''${EMACSLOADPATH:+:}$EMACSLOADPATH"
       alias lean="steam-run lean"
       alias lake="steam-run lake"
      '';
