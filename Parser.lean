2adab5b48890770cd4be2e3b4121691ed192e1be
Florian Engel <engelflorian@posteo.de>
2025-06-09 15:15:08 +0200

    Add abbreviation for parser

--- Parser.lean
+++ Parser.lean
@@ -4,17 +4,19 @@ import HaskellSpec.Names
 
 open Parser Char
 
-def testParser  {α : Type} [inst : BEq α] (p : SimpleParser Substring Char α) (str : String) (exp : α) : Bool :=
+abbrev HaskellParser := SimpleParser Substring Char
+
+def testParser  {α : Type} [inst : BEq α] (p : HaskellParser α) (str : String) (exp : α) : Bool :=
   match Parser.run p str with
     | .ok s r => s == "" && r == exp
     | _ => false
 
-def testParserFails  {α : Type} (p : SimpleParser Substring Char α) (str : String) : Bool :=
+def testParserFails  {α : Type} (p : HaskellParser α) (str : String) : Bool :=
   match Parser.run p str with
     | .ok _ _ => false
     | _ => true
 
-def parseLiteral : SimpleParser Substring Char Source.Literal :=
+def parseLiteral : HaskellParser Source.Literal :=
  Source.Literal.char <$> (char '\'' *> ASCII.alphanum <* char '\'')
  <|> Source.Literal.string <$>
  (char '"' *> ((String.mk ∘ Array.toList ∘ Prod.fst) <$> takeUntil (char '"') ASCII.alphanum ))
@@ -27,31 +29,31 @@ def parseLiteral : SimpleParser Substring Char Source.Literal :=
 #guard testParser parseLiteral "42.42" (.float 42.42)
 #guard testParser parseLiteral "42e42" (.float 42e42)
 
-def parseQualifier : SimpleParser Substring Char Source.Qualifier :=
+def parseQualifier : HaskellParser Source.Qualifier :=
   string "qualified" *> pure Source.Qualifier.qualified
   <|> pure Source.Qualifier.unqualified
 
 #guard testParser parseQualifier "qualified"  .qualified 
 
-def parseModuleName : SimpleParser Substring Char Module_Name :=
+def parseModuleName : HaskellParser Module_Name :=
   (λ c cs => Module_Name.Mk ∘ String.mk ∘ List.cons c $ Array.toList cs ) <$> ASCII.uppercase <*> takeMany ASCII.alphanum
 
 #guard testParser parseModuleName "Mod" (.Mk "Mod")
 #guard testParserFails parseModuleName "mod" 
 
-def parseVariable : SimpleParser Substring Char Variable :=
+def parseVariable : HaskellParser Variable :=
   (λ c cs => Variable.Mk ∘ String.mk ∘ List.cons c $ Array.toList cs ) <$> ASCII.lowercase <*> takeMany ASCII.alphanum
 
 #guard testParser parseVariable "var" (.Mk "var")
 #guard testParserFails parseVariable "Var"
 
-def parseQVarible : SimpleParser Substring Char QVariable :=
+def parseQVarible : HaskellParser QVariable :=
   QVariable.Unqualified <$> parseVariable
  -- Qualified original only exist in target language
 
 #guard testParser parseQVarible "var" (.Unqualified (.Mk "var"))
 
-def parseSpecialDataConstructor : SimpleParser Substring Char Special_Data_Constructor :=
+def parseSpecialDataConstructor : HaskellParser Special_Data_Constructor :=
    char '(' *> dropMany (char ' ') *> char ')' *> pure Special_Data_Constructor.Unit
    <|> Special_Data_Constructor.Tuple <$> (char '(' *> dropMany (char ' ') *> ASCII.parseNat <* dropMany (char ' ') <* char ')')
    <|> char '[' *> dropMany (char ' ') *> char ']' *> pure Special_Data_Constructor.Nil
@@ -65,7 +67,7 @@ def parseSpecialDataConstructor : SimpleParser Substring Char Special_Data_Const
 #guard testParser parseSpecialDataConstructor "[   ]" Special_Data_Constructor.Nil
 #guard testParser parseSpecialDataConstructor ":" Special_Data_Constructor.Cons
 
-def parseConstrutor : SimpleParser Substring Char Constructor :=
+def parseConstrutor : HaskellParser Constructor :=
   (λ c cs => Constructor.Mk ∘ String.mk ∘ List.cons c $ Array.toList cs) <$> ASCII.uppercase <*> takeMany ASCII.alphanum
 
 #guard testParser parseConstrutor "Cons" (Constructor.Mk "Cons")
