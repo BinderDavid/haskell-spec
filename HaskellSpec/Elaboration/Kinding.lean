be27f3ec526598fd85cb805da9c7fb1162ab137d
David Binder <david.binder@uni-tuebingen.de>
2025-06-09 14:11:54 +0200

    Add comments to kinding rules

--- HaskellSpec/Elaboration/Kinding.lean
+++ HaskellSpec/Elaboration/Kinding.lean
@@ -15,6 +15,9 @@ open Source Env SemTy
 
 /-
 Fig. 10 (Kind inference, type expressions)
+```text
+KE ⊢ t : κ
+```
 -/
 inductive ktype : KE → TypeExpression → Kind → Prop where
   | Kind_TVar :
@@ -42,17 +45,68 @@ inductive KindOrdering : SemTy.Kind → SemTy.Kind → Prop where
              → KindOrdering κ₂ κ₂'
              → KindOrdering (SemTy.Kind.Fun κ₁ κ₂) (SemTy.Kind.Fun κ₁' κ₂')
 
-inductive kctDecls : KindEnv → Source.ClassesAndTypes → KindEnv → Prop where
+/--
+Cp. fig 8
+```text
+KE ⊢ ctDecls : KE
+```
+-/
+inductive kctDecls : KE
+                   → Source.ClassesAndTypes
+                   → KE
+                   → Prop where
 
-inductive kgroup : KindEnv → List Source.ClassOrType → KindEnv → Prop where
+/--
+Cp. fig 8
+```text
+KE ⊢ ctDecl₁; … ; ctDeclₙ : KE
+```
+-/
+inductive kgroup : KE
+                 → List Source.ClassOrType
+                 → KE
+                 → Prop where
 
-inductive kctDecl : KindEnv → Source.ClassOrType → KindEnv → Prop where
+/--
+Cp. fig 8
+```text
+KE ⊢ ctDecl : KE
+```
+-/
+inductive kctDecl : KE
+                  → Source.ClassOrType
+                  → KE
+                  → Prop where
 
-inductive kconDecl : KindEnv → Source.ConstructorDecl → Prop where
+/--
+Cp. fig 9
+```text
+KE ⊢ conDecl
+```
+-/
+inductive kconDecl : KE
+                   → Source.ConstructorDecl
+                   → Prop where
 
-inductive ksigs : KindEnv → Source.Signatures → Prop where
+/--
+Cp. fig 9
+```text
+KE ⊢ sigs
+```
+-/
+inductive ksigs : KE
+                → Source.Signatures
+                → Prop where
 
-inductive ksig : KindEnv → Source.Signature → Prop where
+/--
+Cp. fig 9
+```text
+KE ⊢ sig
+```
+-/
+inductive ksig : KE
+               → Source.Signature
+               → Prop where
 
 
 open  Source ClassAssertion Context
@@ -65,15 +119,17 @@ i ∈ [1, n] : KE ⊢^ktype tᵢ : κᵢ
 KE ⊢^kctx C₁ t₁, ... Cₙ tₙ
 ```
 -/
-inductive kctx : KindEnv → Source.Context → Prop where
-  | KIND_CTX {CAS KE} :
+inductive kctx : KE
+               → Source.Context
+               → Prop where
+  | KIND_CTX {CAS ke} :
       (∀ CA K,
         CA ∈ CAS →
-        (KindEnv_Name.C (classAssertionName CA), K) ∈ KE →
-        ktype KE (classAssertionType CA) K
+        (KindEnv_Name.C (classAssertionName CA), K) ∈ ke →
+        ktype ke (classAssertionType CA) K
       )
       → ---------------------------------------------
-      kctx KE (Context.cx CAS)
+      kctx ke (Context.cx CAS)
 
 
 end Kinding
