7765c5256bda232445b34dfc1ad3a67d226cc9c1
David Binder <david.binder@uni-tuebingen.de>
2025-07-17 18:06:38 +0100

    Define max prefix

--- HaskellSpec/Lexer/Rules.lean
+++ HaskellSpec/Lexer/Rules.lean
@@ -7,3 +7,21 @@ Rules
 
 inductive Rule : Type where
   | mk : RE → (String → Token) → Rule
+
+def re : Rule → RE := λ x =>
+  match x with
+  | Rule.mk re _ => re
+
+/-- `p` is a prefix of `z` -/
+def Prefix (p z : List Char) : Prop :=
+  ∃ s, p ++ s = z
+
+/-- `p` is the longest prefix of `z` matching a rule in the set `R` -/
+def MaxPref (R : List Rule) (p z : List Char) : Prop :=
+  /- `p` is a prefix of  `z`-/
+  Prefix p z ∧
+  /- `p` matches against a rule in `R`-/
+  (∃ r ∈ R, Matching (re r) p) ∧
+  /- All longer prefixes don't match -/
+  (∀ p', Prefix p' z ∧ p.length < p'.length →
+         ∀ r' ∈ R, ¬ (Matching (re r') p'))
