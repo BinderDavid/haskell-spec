b79f4431b971db725e5316585de41385dd3b35d7
David Binder <david.binder@uni-tuebingen.de>
2025-07-29 16:25:21 +0100

    Add columnizer (#61)

--- HaskellSpec.lean
+++ HaskellSpec.lean
@@ -16,6 +16,7 @@ import HaskellSpec.Elaboration.Expressions
 import HaskellSpec.Lexer.Rules
 import HaskellSpec.Lexer.RegExp
 import HaskellSpec.Lexer.Brzozowski
+import HaskellSpec.Lexer.Columnizer
 import HaskellSpec.Lexer.Haskell.Tokens
 import HaskellSpec.Lexer.Haskell.Literals
 import HaskellSpec.Lexer.Haskell.Reserved
