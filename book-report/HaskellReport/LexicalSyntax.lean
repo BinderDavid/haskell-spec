fdb784a2b2b97e9f9f6c1b164c61ecb941608376
David Binder <david.binder@uni-tuebingen.de>
2025-08-01 15:20:38 +0100

    Start working on lexical syntax section

--- book-report/HaskellReport/LexicalSyntax.lean
+++ book-report/HaskellReport/LexicalSyntax.lean
@@ -1,5 +1,6 @@
 import VersoManual
 import HaskellReport.Papers
+import HaskellSpec.Lexer.Haskell.Combined
 
 open Verso.Genre Manual
 open Verso.Genre.Manual.InlineLean
@@ -8,4 +9,52 @@ set_option pp.rawOnError true
 
 #doc (Manual) "Lexical Syntax" =>
 
-Description of lexical syntax
+Description of lexical syntax...
+
+# Keywords
+
+# Symbols
+
+# Literals
+
+## Integer Literals
+
+```lean
+#eval lex_haskell "42"
+#eval lex_haskell "0x42"
+#eval lex_haskell "0X42"
+#eval lex_haskell "0o42"
+#eval lex_haskell "0O42"
+```
+
+## Float Literals
+
+```lean
+#eval lex_haskell "42.0"
+```
+
+## Character Literals
+
+```lean
+#eval lex_haskell "'a'"
+```
+
+## String Literals
+
+```lean
+#eval lex_haskell "\"hello world\""
+```
+
+# Comments
+
+## Line Comments
+
+```lean
+#eval lex_haskell "-- some comment"
+```
+
+## Block Comments
+
+```lean
+#eval lex_haskell "{- some comment -}"
+```
