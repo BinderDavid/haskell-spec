2d353ff154a76a1fe8ec332494d90fe7293235bd
David Binder <david.binder@uni-tuebingen.de>
2025-08-01 15:52:14 +0100

    Add text about comments

--- book-report/HaskellReport/LexicalSyntax.lean
+++ book-report/HaskellReport/LexicalSyntax.lean
@@ -47,7 +47,13 @@ Description of lexical syntax...
 
 # Comments
 
+Haskell supports two styles of comments, line comments and block comments.
+
 ## Line Comments
+
+A line comment begins with a sequence of two or more consecutive dashes (e.g. "--") and extends to the following newline.
+The sequence of dashes must not form part of a legal lexeme.
+For example, "-->" or "|--" do not begin a comment, because both of these are legal lexemes; however "--foo" does start a comment.
 
 ```lean
 #eval lex_haskell "-- some comment"
