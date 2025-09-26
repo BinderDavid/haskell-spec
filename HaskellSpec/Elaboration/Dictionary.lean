0d747b449027b240340ada35d8060d9ed0219e1a
David Binder <david.binder@uni-tuebingen.de>
2025-09-26 18:30:26 +0100

    Fix up

--- HaskellSpec/Elaboration/Dictionary.lean
+++ HaskellSpec/Elaboration/Dictionary.lean
@@ -3,6 +3,23 @@ import HaskellSpec.Environments
 import HaskellSpec.SemanticTypes
 
 
+def make_tuple (es : List Target.Expression) : Target.Expression :=
+  sorry
+
+
+
+set_option quotPrecheck false in
+set_option hygiene false in
+notation  "《dict-single》" ie "⊢" e "፥" ca "▪"  => dict_single ie e ca
+
+set_option quotPrecheck false in
+set_option hygiene false in
+notation  "《dict-many》" ie "⊢" es "፥" θ "▪"  => dict_many ie es θ
+
+set_option quotPrecheck false in
+set_option hygiene false in
+notation  "《dict》" ie "⊢" e "፥" θ "▪"  => dict ie e θ
+
 mutual
   inductive dict_single : Env.IE
                         → Target.Expression
@@ -16,13 +33,13 @@ mutual
     | DICT_INST :
       (Env.IE_Entry.DictionaryFromInstanceDeclaration x αs θ Γ χ _) ∈ ie ->
       (τsForαs : SemTy.VarSubst) → (Env.dom τsForαs) = αs →
-      dict_ctx ie e (SemTy.Substitute.substitute τsForαs θ) →
+      dict ie e (SemTy.Substitute.substitute τsForαs θ) →
       τs = Env.rng τsForαs →
       e_target = (Target.Expression.app (match τs with
                                          | [] => Target.Expression.var x
                                          | (t :: ts)  => Target.Expression.typ_app (Target.Expression.var x) (NonEmpty.mk t ts)) e) →
       -------------------------------------------------------------------------------------------------------------------------------
-      dict_single ie _ _
+      dict_single ie e_target ⟨Γ, τs.foldl SemTy.TypeS.App (SemTy.TypeS.TypeConstructor χ)⟩
 
     | DICT_SUPER :
       Env.IE_Entry.ExtractsADictionaryForTheSuperclass x α Γ Γ' ∈ ie →
@@ -30,15 +47,26 @@ mutual
       -----------------------------------------
       dict_single ie (Target.Expression.app (Target.Expression.typ_app (Target.Expression.var x) (singleton τ)) e) ⟨Γ, τ⟩
 
-  inductive dict_ctx : Env.IE
+  inductive dict_many : Env.IE
+                      → List Target.Expression
+                      → SemTy.Context
+                      → Prop where
+    | NIL :
+      ------------------
+      dict_many ie [] []
+
+    | CONS :
+      dict_single ie e c →
+      dict_many ie es cs →
+      ----------------------------
+      dict_many ie (e::es) (c::cs)
+
+  inductive dict : Env.IE
                      → Target.Expression
                      → SemTy.Context
                      → Prop where
     | DICT_TUPLE :
-      dict_ctx _ _ _
+      dict_many ie es θ →
+      -------------------------
+      dict ie (make_tuple es) θ
 end
-
-
-set_option quotPrecheck false in
-set_option hygiene false in
-notation  "《dict》" ie "⊢" e "፥" τ "▪"  => dict ie e τ
