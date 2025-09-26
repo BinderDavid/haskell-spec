4be7cff9c60259a44cdceae82cd93d399cced0b7
David Binder <david.binder@uni-tuebingen.de>
2025-09-26 18:36:40 +0100

    Finish dict

--- HaskellSpec/Elaboration/Dictionary.lean
+++ HaskellSpec/Elaboration/Dictionary.lean
@@ -28,45 +28,45 @@ mutual
     | DICT_VAR :
       Env.IE_Entry.BoundInDictionaryAbstraction v class_name α τs ∈ ie →
       --------------------------------------------------------------------------------------------------------------------------------
-      dict_single ie (Target.Expression.var (QVariable.Unqualified v)) ⟨class_name, τs.foldl SemTy.TypeS.App (SemTy.TypeS.Variable α)⟩
+      《dict-single》ie ⊢ Target.Expression.var (QVariable.Unqualified v) ፥ ⟨class_name, τs.foldl SemTy.TypeS.App (SemTy.TypeS.Variable α)⟩ ▪
 
     | DICT_INST :
       (Env.IE_Entry.DictionaryFromInstanceDeclaration x αs θ Γ χ _) ∈ ie ->
       (τsForαs : SemTy.VarSubst) → (Env.dom τsForαs) = αs →
-      dict ie e (SemTy.Substitute.substitute τsForαs θ) →
+      《dict》ie ⊢ e ፥ (SemTy.Substitute.substitute τsForαs θ) ▪ →
       τs = Env.rng τsForαs →
       e_target = (Target.Expression.app (match τs with
                                          | [] => Target.Expression.var x
                                          | (t :: ts)  => Target.Expression.typ_app (Target.Expression.var x) (NonEmpty.mk t ts)) e) →
       -------------------------------------------------------------------------------------------------------------------------------
-      dict_single ie e_target ⟨Γ, τs.foldl SemTy.TypeS.App (SemTy.TypeS.TypeConstructor χ)⟩
+      《dict-single》ie ⊢ e_target ፥ ⟨Γ, τs.foldl SemTy.TypeS.App (SemTy.TypeS.TypeConstructor χ)⟩ ▪
 
     | DICT_SUPER :
       Env.IE_Entry.ExtractsADictionaryForTheSuperclass x α Γ Γ' ∈ ie →
-      dict_single ie e ⟨Γ', τ⟩ →
-      -----------------------------------------
-      dict_single ie (Target.Expression.app (Target.Expression.typ_app (Target.Expression.var x) (singleton τ)) e) ⟨Γ, τ⟩
+      《dict-single》ie ⊢ e ፥ ⟨Γ', τ⟩ ▪ →
+      -------------------------------------------------------------------------------------------------------------------------
+      《dict-single》ie ⊢ Target.Expression.app (Target.Expression.typ_app (Target.Expression.var x) (singleton τ)) e ፥ ⟨Γ, τ⟩ ▪
 
   inductive dict_many : Env.IE
                       → List Target.Expression
                       → SemTy.Context
                       → Prop where
     | NIL :
-      ------------------
-      dict_many ie [] []
+      --------------------------
+      《dict-many》ie ⊢ [] ፥ [] ▪
 
     | CONS :
-      dict_single ie e c →
-      dict_many ie es cs →
-      ----------------------------
-      dict_many ie (e::es) (c::cs)
+      《dict-single》ie ⊢ e ፥ c ▪ →
+      《dict-many》ie ⊢ es ፥ cs ▪ →
+      ------------------------------------
+      《dict-many》ie ⊢ e::es ፥ c::cs ▪
 
   inductive dict : Env.IE
                      → Target.Expression
                      → SemTy.Context
                      → Prop where
     | DICT_TUPLE :
-      dict_many ie es θ →
-      -------------------------
-      dict ie (make_tuple es) θ
+      《dict-many》ie ⊢ es ፥ θ ▪ →
+      -------------------------------
+      《dict》ie ⊢ make_tuple es ፥ θ ▪
 end
