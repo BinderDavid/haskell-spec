9c268a45a278d2d975abab5ee251f57ef4ce28bf
David Binder <david.binder@uni-tuebingen.de>
2025-09-30 01:27:16 +0100

    Move lcon

--- HaskellSpec/Elaboration/Modules.lean
+++ HaskellSpec/Elaboration/Modules.lean
@@ -232,29 +232,6 @@ inductive condecl : Env.TE
     《condecl》te,θ,τ ⊢ Source.ConstructorDecl.labcon J _ ⇝ Target.ConstructorDecl.labcon J _ ፥ de,ve,le,θ' ▪
 
 
-
-set_option quotPrecheck false in
-set_option hygiene false in
-notation  "《lcon》" ie "," φ "⊢" τ_old "," ue "," τ_new "▪" => lcon ie φ τ_old ue τ_new
-
-/--
-Cp. Fig 22
-```text
-IE, φ ⊢ τ_old, UE, τ_new
-```
--/
-inductive lcon : Env.IE
-               → Env.LabelInfo
-               → SemTy.TypeS
-               → Env.UE
-               → SemTy.TypeS
-               → Prop where
-  | LCON :
-    《dict》ie ⊢ _ ፥ _ ▪ →
-    《dict》ie ⊢ _ ፥ _ ▪ →
-    ------------------------------------------------------
-    《lcon》ie , Env.LabelInfo.mk αs θ ue τ ⊢ _ , ue , _ ▪
-
 set_option quotPrecheck false in
 set_option hygiene false in
 notation  "《sig》" ge "⊢" sign "፥" ve "▪" => sig ge sign ve
